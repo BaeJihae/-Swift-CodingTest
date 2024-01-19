8bb5f1a51cc4b0a0c3d6f60b8073677d2099fead
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-01-19 21:17:26 +0900

    [프로그래머스] 코딩테스트 입문 문제

---
+++ [프로그래머스]/0-프로그래머스 입문/120807_숫자비교하기.swift
@@ -0,0 +1,7 @@
+//
+//  [ 프로그래머스 ] 120807 - 숫자 비교하기
+//
+//  문제 링크
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120807
+//
+func solution(_ num1:Int, _ num2:Int) -> Int { num1 == num2 ? 1 : -1 }
