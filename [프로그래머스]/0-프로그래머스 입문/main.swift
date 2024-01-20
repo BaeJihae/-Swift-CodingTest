c7965b33dc2523917a9a7f006c54796168c83db1
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-01-20 10:54:56 +0900

    [ 프로그래머스 ]

--- [프로그래머스]/0-프로그래머스 입문/main.swift
+++ [프로그래머스]/0-프로그래머스 입문/main.swift
@@ -1,4 +1,5 @@
 //
+//  [ 프로그래머스 ] 120820 - 나이 출력
 //
 //  문제 링크
 //  https://school.programmers.co.kr/learn/courses/30/lessons/120820
--- swift_codingTest/swift_codingTest/main.swift
+++ swift_codingTest/swift_codingTest/main.swift
@@ -1,17 +1,22 @@
 //
-//  [ 프로그래머스 ] 120868 - 삼각형의 완성조건(2)
+//  [ 프로그래머스 ] 120829 - 각도기
 //
 //  문제 링크
-//  https://school.programmers.co.kr/learn/courses/30/lessons/120868
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120829
 //
-
 import Foundation
 
-func solution(_ sides:[Int]) -> Int {
-    var newsides = sides.sorted()
-    var count = (newsides[0]...newsides[1]).filter { $0+newsides[0] > newsides[1] }.count
-    count += (1..<newsides[0]).filter { $0 + newsides[0] > newsides[1]}.count
-    count += (newsides[1]+1..<newsides[0]+newsides[1]).count
-    
-    return count
+func solution(_ angle:Int) -> Int {
+    switch angle {
+        case 0..<90:
+            return 1
+        case 90:
+            return 2
+        case 90..<180:
+            return 3
+        case 180:
+            return 4
+        default:
+            return 0
+    }
 }
