6eddc927f1254fa766a162325d97800e42d5c575
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-02-26 14:48:06 +0900

    [프로그래머스] 월간 코드 챌린지

---
+++ [프로그래머스]/1-연습/12915_두개뽑아서더하기.swift
@@ -0,0 +1,20 @@
+//
+// [프로그래머스] 12915 - 두 개 뽑아서 더하기
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
+//
+import Foundation
+
+// 나의 풀이
+func solution(_ numbers:[Int]) -> [Int] {
+    var answer = [Int]()
+    for i in 0..<numbers.count-1{
+        for j in i+1..<numbers.count{
+            let newN:Int = numbers[i] + numbers[j]
+            if !answer.contains(newN) {
+                answer.append(newN)
+            }
+        }
+    }
+    return answer.sorted()
+}
