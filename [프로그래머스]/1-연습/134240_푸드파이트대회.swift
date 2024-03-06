62868dc025bce016e54e5066cec33372464b0b99
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-06 10:00:20 +0900

    [프로그래머스] Summer/Winter Coding(~2018) 문제

--- [프로그래머스]/1-연습/134240_푸드파이트대회.swift
+++ [프로그래머스]/1-연습/134240_푸드파이트대회.swift
@@ -1,45 +1,17 @@
 //
-// [프로그래머스] 134240 - 푸드 파이트 대회
+// [프로그래머스] 132267 - 콜라문제
 //
-// https://school.programmers.co.kr/learn/courses/30/lessons/134240
+// https://school.programmers.co.kr/learn/courses/30/lessons/132267
 //
 import Foundation
 
 // 나의 풀이
-func solution(_ food:[Int]) -> String {
-    
-    var answer: String = "0"
-    
-    for (i, food) in food.enumerated().reversed() {
-        var j = 0
-        while ( j < food / 2 ){
-            answer = "\(i)" + answer + "\(i)"
-            j += 1
-        }
+func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
+    var n: Int = n
+    var answer: Int = 0
+    while n >= a {
+        answer = answer + ( n / a * b )
+        n = n / a * b + n % a
     }
-    
     return answer
 }
-
-func solution2(_ food:[Int]) -> String {
-    
-    var answer: String = "0"
-    
-    for (i, food) in food.enumerated().reversed() {
-        let addStr = String(repeating: "\(i)", count: food / 2)
-        answer = addStr + answer + addStr
-    }
-    
-    return answer
-}
-
-// 다른 사람의 풀이
-
-func solution3(_ food:[Int]) -> String {
-var result = ""
-for i in food.indices {
-    result += String(repeating: String(i), count: food[i] / 2)
-}
-return result + "0" + result.reversed()
-}
-
