7352f2a4e240889e6e668cae364f946331ceb86f
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-11 10:45:16 +0900

    [level 1] Title: 옹알이 (2), Time: 2.22 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
@@ -1,27 +1,3 @@
-import Foundation
-
-func solution(_ babbling:[String]) -> Int {
-    let strArr = ["aya", "ye", "woo", "ma"]
-
-    var answer = 0
-    func checkWord(_ str: String) -> Bool {
-        var b = str
-
-        for s in strArr {
-            b = b.replacingOccurrences(of: s, with: "-")
-            if b.contains("--") { return false }
-            b = b.replacingOccurrences(of: "-", with: " ")
-
-        }
-
-        return b.replacingOccurrences(of: " ", with: "").isEmpty
-    }
-
-    for babble in babbling {
-        if checkWord(babble) {
-            answer += 1
-        }
-    }
-
-    return answer
+func solution(_ babbling: [String]) -> Int {
+    return babbling.filter { $0.range(of: "^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$", options: .regularExpression) != nil }.count
 }
