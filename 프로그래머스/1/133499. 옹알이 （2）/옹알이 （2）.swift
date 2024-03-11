8e0e3ca66c69ce319d210cbb6b60170fda7235c3
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-11 10:19:41 +0900

    [level 1] Title: 옹알이 (2), Time: 2.24 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
@@ -1,14 +1,27 @@
+import Foundation
+
 func solution(_ babbling:[String]) -> Int {
-    var count: Int = 0
-    for element in babbling {
-        var str = String(element)
-        str = str.replacingOccurrences(of: "aya", with: "1")
-        str = str.replacingOccurrences(of: "ye", with: "2")
-        str = str.replacingOccurrences(of: "woo", with: "3")
-        str = str.replacingOccurrences(of: "ma", with: "4")
-        if Int(str) != nil && !str.contains("11") && !str.contains("22") && !str.contains("33") && !str.contains("44"){
-            count += 1
+    let strArr = ["aya", "ye", "woo", "ma"]
+
+    var answer = 0
+    func checkWord(_ str: String) -> Bool {
+        var b = str
+
+        for s in strArr {
+            b = b.replacingOccurrences(of: s, with: "-")
+            if b.contains("--") { return false }
+            b = b.replacingOccurrences(of: "-", with: " ")
+
         }
-    }    
-    return count
+
+        return b.replacingOccurrences(of: " ", with: "").isEmpty
+    }
+
+    for babble in babbling {
+        if checkWord(babble) {
+            answer += 1
+        }
+    }
+
+    return answer
 }
