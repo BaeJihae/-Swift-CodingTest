cb0f141047f08fa9051760c47f0c8d4859526b0d
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-20 15:53:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.swift
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.swift
@@ -2,22 +2,31 @@ import Foundation
 
 func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     
-    // 레포트를 Dict로 저장
-    var reportDict = [String: [String]]()
+    // 레포트를 array로 저장
+    var reportDic = [String:[String]]()
     // 신고받은 횟수 저장
-    var reportedUserDict = [String: Int]()
-    
-    for str in Set(report){
-        let array = str.split(separator: " ").map{ String($0) }
+    var reportedUserDict = [String:Int]()
+
+    for str in Set(report) {
+        var reportArr = str.split(separator: " ").map{ String($0) }
         
-        reportDict[array[0]] = (reportDict[array[0]] ?? []) + [array[1]]
-        reportedUserDict[array[1]] = (reportedUserDict[array[1]] ?? 0) + 1
+        // 신고받은 유저만 reportedUserDict에 저장
+        reportedUserDict[reportArr[1]] = (reportedUserDict[reportArr[1]] ?? 0) + 1
+        
+        // 레포트의 내용을 reportDic에 저장
+        reportDic[reportArr[0]] = (reportDic[reportArr[0]] ?? []) + [reportArr[1]]
         
     }
     
-    return id_list.map{ id in
-        return (reportDict[id] ?? []).reduce(0) {
-            $0 + ((reportedUserDict[$1] ?? 0 ) >= k ? 1 : 0)
-        }
+    reportedUserDict.forEach{ 
+        if $0.value < k {
+            reportedUserDict[$0.key] = nil
+        } 
+    }
+    
+    return id_list.map { id in
+        (reportDic[id] ?? []).filter{
+            reportedUserDict.keys.contains($0)
+        }.count
     }
 }
