4f3f7d561b6ae546dfb51e8c0005ffd35320ff4a
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-21 08:56:31 +0900

    코딩테스트

--- swift_codingTest/swift_codingTest/main.swift
+++ swift_codingTest/swift_codingTest/main.swift
@@ -6,22 +6,34 @@
 
 import Foundation
 
-func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
-    
-    // 0의 개수 구하기
-    let number0 = lottos.filter{ $0 == 0 }.count
-    
-    // 현재 순위
-    var rank = 7
-    
-    // 일치하는 값
-    for i in win_nums {
-        if lottos.contains(i) {
-            rank -= 1
-        }
-    }
-    
-    let best = rank - number0
-    
-    return [ best == 7 ? 6 : best , rank == 7 ? 6 : rank ]
+
+func solution1(_ s:String) -> Bool
+{
+    let s = s.lowercased()
+    return s.filter{ $0 == "p" }.count == s.filter{ $0 == "y" }.count
 }
+
+func solution2(_ s:String) -> Bool
+{
+    let string = s.lowercased()
+    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
+}
+
+var timeElapsed1: UInt64 = 0
+var timeElapsed2: UInt64 = 0
+
+for _ in 1...100 {
+    let start1 = DispatchTime.now()
+    print(solution1("pPooweljdslfnxocvhzethksrEFLKSJODIFJSEVBVNJ0UKKGLHNJIVJttyryertyguyqieuyuweyrutsrtyhoyY"))
+    let end1 = DispatchTime.now()
+    
+    let start2 = DispatchTime.now()
+    print(solution2("pPooweljdslfnxocvhzethksrEFLKSJODIFJSEVBVNJ0UKKGLHNJIVJttyryertyguyqieuyuweyrutsrtyhoyY"))
+    let end2 = DispatchTime.now()
+    
+    timeElapsed1 += ( end1.uptimeNanoseconds - start1.uptimeNanoseconds )
+    timeElapsed2 += ( end2.uptimeNanoseconds - start2.uptimeNanoseconds )
+}
+
+print("첫번째 함수는 평균 \(timeElapsed1/100)나노초 걸렸습니다.")
+print("두번째 함수는 평균 \(timeElapsed2/100)나노초 걸렸습니다.")
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -1,4 +1,28 @@
 import Foundation
 
-func solution(_ s:String) -> Bool
-{  s.filter{ $0 == "p" || $0 == "P" }.count == s.filter{ $0 == "y" || $0 == "Y" }.count }
+
+func solution1(_ s:String) -> Bool
+{
+    let string = s.lowercased()
+    s.filter{ $0 == "p" }.count == s.filter{ $0 == "y" }.count
+}
+
+func solution2(_ s:String) -> Bool
+{
+    let string = s.lowercased()
+    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
+}
+
+let start1 = DispatchTime.now()
+solution1("pPooweljdslfnxocvhzethksrottyryertyguyqieuyuweyrutsrtyhoyY")
+let end1 = DispatchTime.now()
+
+let start2 = DispatchTime.now()
+solution2("pPooweljdslfnxocvhzethksrottyryertyguyqieuyuweyrutsrtyhoyY")
+let end2 = DispatchTime.now()
+
+let timeElapsed1 = end1.uptimeNanoseconds - start1.uptimeNanoseconds
+let timeElapsed2 = end2.uptimeNanoseconds - start2.uptimeNanoseconds
+
+print("첫번째 함수는 \(timeElapsed1)나노초 걸렸습니다.")
+print("두번째 함수는 \(timeElapsed2)나노초 걸렸습니다.")
