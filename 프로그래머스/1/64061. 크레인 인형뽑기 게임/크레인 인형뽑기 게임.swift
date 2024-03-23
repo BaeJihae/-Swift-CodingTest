c7fb6bbbd1c9243a8cdfa061c7d0c28d4436b43f
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-23 20:48:03 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.56 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
@@ -1,36 +1,28 @@
 import Foundation
 
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    
-    // board를 행의 값을 key로 받은 Dictionary로 전환
-    var boardDic = [Int: [Int]]()
-    
-    // moves에 따른 인형 인덱스를 넣을 배열
-    var stack = [Int]()
-    
-    // 정답
     var count = 0
-    
-    // board -> Dic
-    (0..<board.count).forEach{ i in
-        boardDic[i+1] = (0..<board.count).filter{ board[$0][i] != 0 }
-                                         .map{ board[$0][i] }
-    }
-    
-    for move in moves {
-        var calum = boardDic[move] ?? []
-        guard let doll = calum.first else { continue }
-        
-        calum.removeFirst()
-        boardDic[move] = calum
-        
-        if ( stack.last ?? 0 ) == doll {
-            stack.removeLast()
-            count += 2
-        } else {
-            stack.append(doll)
+    var stacks: [[Int]] = Array(repeating: [], count: board.count)
+    var bucket: [Int] = []
+
+    board.reversed().forEach {
+        $0.enumerated().forEach {
+            if $0.1 != 0 {
+                stacks[$0.0].append($0.1)
+            }
         }
-        
     }
+
+    moves.forEach {
+        if let doll = stacks[$0-1].popLast() {
+            if let last = bucket.last, last == doll {
+                bucket.removeLast(1)
+                count += 2
+            } else {
+                bucket.append(doll)
+            }
+        }
+    }
+
     return count
 }
