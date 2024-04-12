5c33cefb277e94c46acfe4574f4548eaae6cda60
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-12 09:33:46 +0900

    [level 2] Title: 귤 고르기, Time: 229.95 ms, Memory: 24.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.swift
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.swift
@@ -1,20 +1,6 @@
-import Foundation
-
-func solution(_ k:Int, _ tangerine:[Int]) -> Int {
-    
-    var dic = [Int:Int]()
-    var k = k, count = 0
-    
-    for i in tangerine {
-        dic[i] = ( dic[i] ?? 0 ) + 1
-    }
-    
-    for i in dic.values.sorted(by: >){
-        k -= i
-        count += 1
-        if k <= 0 {
-            return count
-        }
-    }
-    return 0
+func solution(_ k: Int, _ tangerine: [Int]) -> Int {
+    return Dictionary(grouping: tangerine) { $0 }.values
+        .sorted { $0.count > $1.count }
+        .reduce((0, 0)) { acc, array in acc.1 >= k ? acc : (acc.0 + 1, acc.1 + array.count) }
+        .0
 }
